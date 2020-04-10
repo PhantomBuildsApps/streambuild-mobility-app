46d46b3f65443e93921fb42fcc16f3b73aa9ef06
Sander Rõuk <sander@rouk.io>
2020-04-10 16:43:18 +0300

    Initialise project

---
+++ MobiltyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobiltyApp
+//
+//  Created by Sander Rõuk on 10/04/2020.
+//  Copyright © 2020 Rouk OÜ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# MobilityApp
+Mobility app is a sample first app used to teach iOS app building.
